04ec87bb2fb359a8ec612e5025c0b5194aab85a6
anisha iyer <anishaiyer27@gmail.com>
2024-04-16 21:08:47 -0700

    updates to split.py, add mp3 and mp4 to gitignore

--- Pre-processing/split.py
+++ Pre-processing/split.py
@@ -2,23 +2,41 @@
 split.py
 
 Simple script that reads in a mouse behavior video in MP4 format and produces several resultant videos each in 30 second segments.
+
+
+
 Author: Anisha Iyer
 """
 
 # TODO: install a JDK
 
 from moviepy.editor import VideoFileClip
+import argparse
+import os
 
-filename = "midterm2review.mp4" # hardcoded filename with current behavior video file (temporarily)
-file_loc = "/Users/anishaiyer/Downloads/" # hardcoded filepath
+parser = argparse.ArgumentParser("splitting script")
+parser.add_argument("filename", help="Filename of video without path", type=str)
+parser.add_argument("path_to_file", help="Path to file without filename", type=str)
+#parser.add_argument("filepath", help="Full filepath to this video locally", type=str)
+args = parser.parse_args()
 
-video = VideoFileClip(file_loc+filename)
+if args.filename:
+    filename = args.filename
+    file_loc = args.path_to_file + "/"
+else:
+    filename = "" # hardcoded filename with current behavior video file (temporarily)
+    file_loc = "/Users/anishaiyer/Downloads/" # hardcoded filepath
+filepath = file_loc+filename
+
+video = VideoFileClip(filepath)
 ## get duration of video in seconds via:
 duration = video.duration
 
 segment_duration = 30
 
 num_segments = int(duration // segment_duration)
+
+#inner_path = file_loc + "clips/"
 
 for i in range(num_segments):
     start_time = i * segment_duration
